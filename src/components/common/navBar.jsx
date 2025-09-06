415b60975ecbc12037119a6d68c804bb2ed0ebf2
Rithvik <rithvikbng@gmail.com>
2025-09-06 20:29:47 +0530

    nav bar updates

--- src/components/common/navBar.jsx
+++ src/components/common/navBar.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./styles/navBar.css";
 
 const NavBar = (props) => {
 	const { active } = props;
+	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
 	return (
 		<React.Fragment>
@@ -12,50 +13,44 @@ const NavBar = (props) => {
 				<nav className="navbar">
 					<div className="nav-background">
 						<ul className="nav-list">
-							<li
-								className={
-									active === "home"
-										? "nav-item active"
-										: "nav-item"
-								}
-							>
+							<li className={active === "home" ? "nav-item active" : "nav-item"}>
 								<Link to="/">Home</Link>
 							</li>
-							<li
-								className={
-									active === "about"
-										? "nav-item active"
-										: "nav-item"
-								}
-							>
+							<li className={active === "about" ? "nav-item active" : "nav-item"}>
 								<Link to="/about">About</Link>
 							</li>
-							<li
-								className={
-									active === "projects"
-										? "nav-item active"
-										: "nav-item"
-								}
-							>
+							<li className={active === "projects" ? "nav-item active" : "nav-item"}>
 								<Link to="/projects">Projects</Link>
 							</li>
-							<li
-								className={
-									active === "articles"
-										? "nav-item active"
-										: "nav-item"
-								}
-							>
+							<li className={active === "articles" ? "nav-item active" : "nav-item"}>
 								<Link to="/articles">Articles</Link>
 							</li>
-							<li
-								className={
-									active === "contact"
-										? "nav-item active"
-										: "nav-item"
-								}
-							>
+							<li className={active === "contact" ? "nav-item active" : "nav-item"}>
 								<Link to="/contact">Contact</Link>
+							</li>
+
+							{/* Dropdown for Resume */}
+							<li 
+								className="nav-item dropdown"
+								onMouseEnter={() => setIsDropdownOpen(true)}
+								onMouseLeave={() => setIsDropdownOpen(false)}
+							>
+								<span className="nav-link">
+									Download my Resume ▾
+								</span>
+								{isDropdownOpen && (
+									<ul className="dropdown-menu">
+										<li>
+											<a href="/resumes/dev_resume.pdf" download>Developer Resume</a>
+										</li>
+										<li>
+											<a href="/resumes/designer_resume.pdf" download>Designer Resume</a>
+										</li>
+										<li>
+											<a href="/resumes/manager_resume.pdf" download>Manager Resume</a>
+										</li>
+									</ul>
+								)}
 							</li>
 						</ul>
 					</div>
